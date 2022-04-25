3d930d5b0045e244f241629b1625c6b4e80fe51e
Yang <yangboy9711@gmail.com>
2022-04-25 23:16:56 +0800

    添加统一异常处理

--- blog-api/src/main/java/website/ubook/handler/AllExceptionHandler.java
+++ blog-api/src/main/java/website/ubook/handler/AllExceptionHandler.java
@@ -1,4 +1,23 @@
 package website.ubook.handler;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import website.ubook.vo.Result;
+
+
+/**
+ * 对加了Controller注解的方法进行拦截助理 AOP的实现
+ */
+@ControllerAdvice
 public class AllExceptionHandler {
+
+    //进行异常处理,处理Exception.class的异常
+    @ExceptionHandler(Exception.class)
+    @ResponseBody//返回json数据
+    public Result doException(Exception ex) {
+        ex.printStackTrace();
+        return Result.fail(-999, "系统异常");
+    }
+
 }
